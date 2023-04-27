a0d7145673b599ae8cde1f38cc9fa74229188927
leonardo3909 <127439867+leonardo3909@users.noreply.github.com>
2023-04-27 15:02:09 -0500

    Initial commit

---
+++ src/main/java/co/edu/umanizales/tads_prog4/TadsProg4Application.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.tads_prog4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadsProg4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadsProg4Application.class, args);
+    }
+
+}
